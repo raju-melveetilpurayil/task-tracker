dec8989f4b628b6f56b5d2d594732329dae9b169
Raju Melveetilpurayil <codename.raju@gmail.com>
2023-01-25 00:32:30 +0000

    [~] Tasks and Task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import Header from "./componets/Header";
 import Tasks from "./componets/Tasks";
-function App() {
+import { useState } from 'react'
+
+function App() 
+{
+  const [tasks,setTasks]=useState( [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true
+    }, {
+        id: 3,
+        text: 'Grocery Shopping',
+        day: 'Feb 9th at 6:30pm',
+        reminder: true
+    }, {
+        id: 4,
+        text: 'Ryan Birthday',
+        day: 'Feb 21st at 4:30pm',
+        reminder: true
+    }
+])
+//delete task
+const deleteTask=(id)=>{
+ setTasks(tasks.filter(x=>x.id!==id));
+}
+const toggleReminder=(id)=>{
+setTasks(tasks.map((task)=>task.id==id?{...task,reminder:!task.reminder}:task))
+}
   return (
     <div className="container">
       <Header></Header>
-      <Tasks/>
+      {tasks.length>0?(<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>):('No Task to show')}
     </div>
   );
 }
--- src/componets/Tasks.js
+++ src/componets/Tasks.js
@@ -1,34 +1,13 @@
 import React from 'react'
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    }, {
-        id: 3,
-        text: 'Grocery Shopping',
-        day: 'Feb 9th at 6:30pm',
-        reminder: true
-    }, {
-        id: 4,
-        text: 'Ryan Birthday',
-        day: 'Feb 21st at 4:30pm',
-        reminder: true
-    }
-]
-const Tasks = () => {
+import Task from './Task'
+
+
+const Tasks = ({tasks,onDelete,onToggle}) => {
     return(
         <>
             {
                 tasks.map((task) => (
-                    <h3 key={task.id}>{task.text}</h3>
+                    <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
             ))}
         </>
     )
